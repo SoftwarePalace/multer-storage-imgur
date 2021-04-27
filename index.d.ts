a69eeebe0f06ea939294bfe357cb63429409ac62
capure <stolarek83@gmail.com>
2021-04-27 11:24:57 +0200

    :bug: Fixed the typings

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
-declare function setupImgurStorage(opts: {clientId: string}): {
-  _handleFile: (req: any, file: any, cb: any) => any,
-  _removeFile: (req: any, file: any, cb: any) => void
+declare module "multer-storage-imgur" {
+  export default function setupImgurStorage(opts: {
+    clientId: string;
+  }): {
+    _handleFile: (req: any, file: any, cb: any) => any;
+    _removeFile: (req: any, file: any, cb: any) => void;
+  };
 }
-
-module.exports = setupImgurStorage;
